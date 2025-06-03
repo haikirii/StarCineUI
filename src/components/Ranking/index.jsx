00bee7958fc9cbc923eae3fb3950295a314c7da2
haikirii <snufles53@gmail.com>
2025-06-03 18:57:28 -0400

    merge main

--- src/components/Ranking/index.jsx
+++ src/components/Ranking/index.jsx
@@ -6,10 +6,6 @@ const Ranking = ({ data }) => {
     <div className={styles.rankingGrid}>
       {data.map((item, index) => (
         <div className={styles.card} key={index}>
-<<<<<<< HEAD
-=======
-          
->>>>>>> c2fd935a467aefaae9fd3678ac68d85c602b0e61
           <div className={styles.rank}>{item.rank}</div>
           <img src={item.src} alt={item.title} className={styles.image} />
           <h3 className={styles.title}>{item.title}</h3>
